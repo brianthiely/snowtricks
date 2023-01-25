6bc1f37067e1a98fb303980534ee1bd5589e5c9a
Brian <brian.thiely@gmail.com>
2023-01-25 18:58:22 +0100

    #3 Homepage with tricks created

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+
+        $tricks = $trickRepository->findAll();
+        $pictures = $trickRepository->findAll();
+        return $this->render('homepage/index.html.twig', compact('tricks', 'pictures'));
     }
 }
